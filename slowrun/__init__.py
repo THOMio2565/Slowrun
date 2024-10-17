85bb31b18af01bfe27e45df6d84b5d06620c1e36
THOMio2565 <thomio.lacroix@gmail.com>
2024-10-17 16:21:29 +0200

    Fuck it we ball
    I don't have any fucking idea of what's happening

--- slowrun/__init__.py
+++ slowrun/__init__.py
@@ -1,14 +1,30 @@
 import flask as fl
+import sqlite3
+from pathlib import Path
+
+db = Path(__file__).parents[1] / 'slowrun.db'
+
+
+def get_connection():
+    """Get connection to database."""
+    connection = sqlite3.connect(db, detect_types=sqlite3.PARSE_DECLTYPES)
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+    cursor.execute('PRAGMA foreign_keys')
+    cursor.close()
+    return connection
 
 app = fl.Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
 
-@app.route("/index")
-def accueil_render():
-    return fl.render_template('index.html')
+@app.route("/")
+def index_render():
+    cursor = get_connection().cursor()
+    games = cursor.execute('''
+            SELECT *
+            FROM game
+        ''').fetchall()
+    return fl.render_template('index.html', games=games)
 
 @app.route("/rankings")
 def rankings_render():
