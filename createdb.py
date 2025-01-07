8597ea598db2319293820fb95dcc340c787b265b
THOMio2565 <thomio.lacroix@gmail.com>
2025-01-07 11:43:39 +0100

    [UPDATE] createdb.py
    Automatized the creation and deletion of the .db files.
    Had to completely change test.sql as we were inserting values into nothing.
    Essentially I copied the code from slowrun.sql into test.sql

--- createdb.py
+++ createdb.py
@@ -1,9 +1,33 @@
 import sqlite3
 from pathlib import Path
 
-db = sqlite3.connect('slowrun.db')
+db_slowrun = Path('slowrun.db')
+db_test = Path('test.db')
 sql = Path('slowrun/database/model.sql').read_text()
 test = Path('slowrun/database/test.sql').read_text()
-db.cursor().executescript(sql)
-db.cursor().executescript(test)
-db.commit()
+
+def create_db_file(db_connection, sql_script, db_path): # db_connection is found as "db" in the if statements; sql_script calls the right .sql file to execute when creating the database, db_path refers to the file name on creation
+    with db_connection:
+        db_connection.executescript(sql_script)
+        print(f"Database '{db_path}' has been initialized.")
+
+def delete_db_file(db_connection, db_path):
+    db_connection.close()  # Closes the connection to the database so the file can be deleted
+    if db_path.exists():
+        db_path.unlink()  # unlink is used to delete the file
+        print(f"Database file '{db_path}' has been deleted.")
+    else:
+        print(f"Database file '{db_path}' does not exist.")
+
+
+if db_slowrun.exists():
+    db = sqlite3.connect(db_slowrun)  
+    delete_db_file(db, db_slowrun)   
+db = sqlite3.connect(db_slowrun)      
+create_db_file(db, sql, db_slowrun)
+
+if db_test.exists():
+    db = sqlite3.connect(db_test)    
+    delete_db_file(db, db_test)     
+db = sqlite3.connect(db_test)        
+create_db_file(db, test, db_test)
--- slowrun/database/test.sql
+++ slowrun/database/test.sql
@@ -1,41 +1,40 @@
-INSERT INTO game (name, count, date)
-VALUES
-("NES Tetris", 4, "1989-05-06"),
-("Minecraft", 3, "2011-11-18"),
-("Trackmania 2020", 2, "2020-07-01"),
-("Brawl Stars", 1, "2018-12-12"),
-("Call Of Duty: Black Ops 6", 0, "2024-10-25");
+CREATE TABLE game(
+    id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    count INTEGER,
+    date DATE NOT NULL
+);
 
-INSERT INTO user (name, date)
-VALUES
-("Toinoufu", "2023-06-10"),
-("THOMio", "2023-07-05"),
-("Dianakolo", "2023-12-03");
+CREATE TABLE user (
+    id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    date DATE NOT NULL
+);
 
-INSERT INTO slowrun (time, date, register_id)
-VALUES
-(4600,"2024-02-02", 1),
-(7200, "2024-03-25", 2),
-(10800, "2024-04-29", 3);
 
-INSERT INTO register (game_id, user_id)
-VALUES
-(5, 3),
-(1, 2),
-(2, 1);
+CREATE TABLE slowrun (
+    id INTEGER PRIMARY KEY NOT NULL,
+    time INTEGER NOT NULL,
+    date DATE NOT NULL,
+    register_id INTEGER NOT NULL REFERENCES register (id)
+);
 
-INSERT INTO news (title, game_id, user_id)
-VALUES
-("Lag strat on level 5", 1, 2),
-("Going for another category mid-run", 5, 3),
-("New seed for a slowrun", 2, 1),
-("Test-icule", 1, 1);
 
-INSERT INTO categories (name, game_id)
-VALUES
-("Rebirth", 1),
-("Maxout", 1),
-("Rollover", 1),
-("Any%", 2),
-("All advancement", 2),
-("how did we get here", 2);
+CREATE TABLE register (
+    id INTEGER PRIMARY KEY NOT NULL,
+    game_id INTEGER NOT NULL REFERENCES game (id),
+    user_id INTEGER NOT NULL REFERENCES user (id)
+);
+
+CREATE TABLE news (
+    id INTEGER PRIMARY KEY NOT NULL,
+    title TEXT NOT NULL,
+    game_id INTEGER NOT NULL REFERENCES game (id),
+    user_id INTEGER NOT NULL REFERENCES user (id)
+);
+
+CREATE TABLE categories (
+    id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    game_id INTEGER NOT NULL REFERENCES game (id)
+);
